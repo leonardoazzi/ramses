6008de611daaad9c1d03d88520e281770105c280
Leonardo Azzi Martins <leoazzim@gmail.com>
2022-07-18 02:39:07 -0300

    Criando pseudo-código

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # ramses
+
+
+## Pseudo código
+
+int dataVec = [14]
+
+int *navVec = [3]; //V,E,D
+
+navVec = dataVec;
+
+if(V < 0){
+    V = 
+} else{
+
+}
---
+++ pseudo.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    int dataVec = [14]
+
+    int *navVec = [3]; //V,E,D
+
+    navVec = dataVec;
+
+    if(V < 0){
+        V = 
+    } else{
+
+    }
+
+}
+
