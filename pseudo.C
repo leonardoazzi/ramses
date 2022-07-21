9ef579bbcb756ce353b6bd6ca5301c3e9947ca4f
Leonardo Azzi Martins <leoazzim@gmail.com>
2022-07-21 20:44:04 -0300

    Proposta do trabalho implementada em C

--- pseudo.C
+++ pseudo.C
@@ -1,16 +1,35 @@
 #include <stdio.h>
 
+#define V 0
+#define E 1
+#define D 2
+
 int main(){
 
-    int dataVec = [14]
+    int dataVec[14] = {-1,-1,5,3,4,100,6,7,8,9,10,11,12,13};
 
-    int *navVec = [3]; //V,E,D
+    int *navVec[3];
 
-    navVec = dataVec;
+    int i = 0;
 
-    if(V < 0){
-        V = 
-    } else{
+    navVec[V] = &dataVec[i]; // V
+    navVec[E] = &dataVec[i+1]; // E
+    navVec[D] = &dataVec[i+2]; // D
+
+    printf("Indice: %d \n", i);
+
+    printf("V: %d \t E: %d \t D: %d \n", *navVec[V], *navVec[E], *navVec[D]);
+
+    if(*navVec[0] < 0){
+        i = *navVec[2]; // D
+
+        navVec[V] = &dataVec[i]; // V
+        navVec[E] = &dataVec[i+1]; // E
+        navVec[D] = &dataVec[i+2]; // D 
+
+        printf("Indice: %d \n", i);
+
+        printf("V: %d \t E: %d \t D: %d \n", *navVec[V], *navVec[E], *navVec[D]);
 
     }
 
